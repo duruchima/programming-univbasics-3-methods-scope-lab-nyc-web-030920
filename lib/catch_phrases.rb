644c21ae2b38cf1b7f194ff25181b988fbd706a3
Duncan Uruchima <duruchima1@gmail.com>
2020-02-21 18:00:45 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,3 +7,10 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+def link
+  hey = "It's Dangerous To Go Alone! Take THis."
+  puts hey
+end
+def any_phrase(phrase)
+  puts "#{phrase}"
+end
